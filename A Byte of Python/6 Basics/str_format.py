d835ab8b9483fd0a9a915bb6b0be590f83ee1ab0
Jerrison Li <jll09c@my.fsu.edu>
2016-03-12 21:02:22 -0800

    first commit

---
+++ A Byte of Python/6 Basics/str_format.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Mar 12 18:23:22 2016
+
+@author: jerri
+"""
+
+age = 20
+name = 'Swaroop'
+
+print('{} was {} years old when he wrote this book'.format(name, age))
+print('Why is {0} playing with that python?'.format(name))
+
+print('{0:_^11}'.format('hello'))
+
+print('What\' your name?')
+
+print('What\' your\\ name?')
+
+print("This is the first sentence. \
+This is the second sentence.")
+
+
+print("""This is the first sentence. \
+This is the second sentence.""")
+
+
+print(r'Newlines are indicated by \n')
---
+++ A Byte of Python/6 Basics/var.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Mar 12 18:41:33 2016
+
+@author: jerri
+"""
+
+i = 5
+print(i)
+i = i + 1
+print(i)
+
+s = '''This is a multi-line string.
+This is the second line.'''
+print(s)
