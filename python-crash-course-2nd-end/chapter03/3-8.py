477ce548f7cda7913e70f8066ed15ead0990dd68
Jerrison Li <jerrisonli@gmail.com>
2020-08-29 11:12:41 -0700

    started dictionary chapter

---
+++ python-crash-course-2nd-end/chapter03/3-8.py
@@ -0,0 +1,14 @@
+locations = ["Croatia", "Hungary", "Japan", "Korea", "Italy"]
+print(f"locations {locations}")
+print(f"sorted list: {sorted(locations)}")
+print(
+    f"locations, again: {locations}, notice that the list goes back to its original order"
+)
+
+locations.reverse()
+print(f"locations, reversed: {locations}")
+print(
+    f"checking locations now: {locations}, notice that the order of the locations is changed permanently"
+)
+
+players = ["charles", "martina", "michael", "florence", "eli"]
