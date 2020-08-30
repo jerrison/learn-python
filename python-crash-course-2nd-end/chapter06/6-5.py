1f746f6f2258d7d25e7bf12fd378ac6816140e49
Jerrison Li <jerrisonli@gmail.com>
2020-08-29 21:50:33 -0700

    finished chapter 07

--- python-crash-course-2nd-end/chapter06/6-5.py
+++ python-crash-course-2nd-end/chapter06/6-5.py
@@ -1,6 +1,6 @@
 rivers = {"nile": "egypt", "han": "china", "delta": "united states of america"}
 
-print(f"The following is a list of rivers and the country they flow through: ")
+print(f"The following is a list of rivers and the country they flow through:  ")
 for river, country in rivers.items():
     print(f"The {river.title()} runs through {country.title()}.")
 
