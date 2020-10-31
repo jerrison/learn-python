2b0948fa84e552f2dfb1dea2c6146864156517ca
Jerrison Li <jerrisonli@gmail.com>
2020-10-31 00:20:03 +0000

    can't get relative imports using dot notation to work

--- python-crash-course-2nd-end/chapter11/test_name_function.py
+++ python-crash-course-2nd-end/chapter11/test_name_function.py
@@ -1,5 +1,6 @@
 import unittest
-from name_function import get_formatted_name
+
+from .name_function import get_formatted_name
 
 
 class NamesTestCase(unittest.TestCase):
@@ -15,6 +16,15 @@ class NamesTestCase(unittest.TestCase):
         """
         formatted_name = get_formatted_name("janis", "joplin")
         self.assertEqual(formatted_name, "Janis Joplin")
+
+    def test_first_last_middle_name(
+        self,
+    ):
+        """
+        Do names like 'Wolfgang Amadeus Mozar' work?
+        """
+        formatted_name = get_formatted_name("wolfgang", "mozart", "amadeus")
+        self.assertEqual(formatted_name, "Wolfgang Amadeus Mozart")
 
 
 if __name__ == "__main__":
